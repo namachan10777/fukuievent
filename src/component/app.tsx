f2bc80fe544467dc4909bc22c0f8b35d1412810a
namachan10777 <namachan10777@gmail.com>
2018-06-06 23:53:09 +0900

    add appbar

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -4,6 +4,18 @@ import * as ReactRedux from 'react-redux';
 import * as PropTypes from 'prop-types';
 import { withStyles, WithStyles } from '@material-ui/core/styles';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import RightIcon from '@material-ui/icons/KeyboardArrowRight';
+import LeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import Input from '@material-ui/core/Input';
+
+import SearchBar from 'material-ui-search-bar';
+
 import * as Store from '../store';
 import * as Action from '../actions';
 import * as AppModule from '../module/app';
@@ -13,6 +25,8 @@ export interface AppProps {
 }
 
 const styles = {
+	leftButton: {},
+	rightButton: {}
 };
 
 type ClassNames = keyof typeof styles;
@@ -20,9 +34,23 @@ type ClassNames = keyof typeof styles;
 const AppComponent = withStyles(styles)<AppProps>(
 	(props: AppProps & WithStyles<ClassNames>) => {
 		const classes = props.classes;
-		return (
-			<div>Hello React</div>
-		);
+		if (!props.state.zoom.isEmpty) {
+			return (<div>Full</div>);
+		}
+		else {
+			let appbar = (<AppBar position='static'>
+				<Toolbar>
+					<IconButton className={classes.leftButton} color='inherit' aria-label='Menu'><LeftIcon/></IconButton>
+					<IconButton className={classes.rightButton} color='inherit' aria-label='Menu'><RightIcon/></IconButton>
+					<Input id='search' />
+				</Toolbar>
+			</AppBar>);
+			return (
+				<div>
+					{appbar}
+				</div>
+			);
+		}
 	}
 );
 
