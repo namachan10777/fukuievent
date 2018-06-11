2b80fe1449e3989beb62c3a4a2bfbe1c59151f29
namachan10777 <namachan10777@gmail.com>
2018-06-12 01:31:20 +0900

    improve appbar ui

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -14,6 +14,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import RightIcon from '@material-ui/icons/KeyboardArrowRight';
 import LeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import CloseIcon from '@material-ui/icons/Close';
+import SearchIcon from '@material-ui/icons/Search';
 import Input from '@material-ui/core/Input';
 
 import * as Store from '../store';
@@ -52,8 +53,41 @@ export interface AppProps {
 }
 
 const styles = {
-	leftButton: {},
-	rightButton: {},
+	appbar: {
+		flexGrow: 1
+	},
+	leftButton: {
+		marginLeft: '3vw',
+		marginRight: 0,
+	},
+	rightButton: {
+		marginLeft: 0,
+		marginRight: 'auto'
+	},
+	searchBar: {
+		marginRight: 0,
+		marginLeft: 'auto' 
+	},
+	searchIcon: {
+		marginLeft: 'auto',
+		marginRight: 0
+	},
+	x10Btn: {
+		marginRight: 0,
+		marginLeft: 'auto'
+	},
+	x30Btn: {
+		marginRight: 0,
+		marginLeft: '1vh'
+	},
+	allBtn: {
+		marginRight: 0,
+		marginLeft: '1vh'
+	},
+	filterBtn: {
+		marginRight: 0,
+		marginLeft: '3vh'
+	},
 };
 
 type ClassNames = keyof typeof styles;
@@ -61,18 +95,22 @@ type ClassNames = keyof typeof styles;
 const AppComponent = withStyles(styles)<AppProps>(
 	(props: AppProps & WithStyles<ClassNames>) => {
 		const classes = props.classes;
-
 		let appbar = (<AppBar position='fixed'>
 			<Toolbar>
 				<IconButton className={classes.leftButton} color='inherit' aria-label='Menu'
 					onClick={() => props.backPage()}><LeftIcon/></IconButton>
 				<IconButton className={classes.rightButton} color='inherit' aria-label='Menu'
 					onClick={() => props.prevPage()}><RightIcon/></IconButton>
+				<SearchIcon className={classes.searchIcon}/>
 				<Input id='search' onChange={(e: any) => props.search(e.target.value)}/>
-				<Button color='inherit' onClick={() => props.changeDStyle(Store.DisplayStyle.X10)}>x10</Button>
-				<Button color='inherit' onClick={() => props.changeDStyle(Store.DisplayStyle.X30)}>x30</Button>
-				<Button color='inherit' onClick={() => props.changeDStyle(Store.DisplayStyle.All)}>All</Button>
-				<Button color='inherit' onClick={() => props.openFilterDialog()}>絞り込み</Button>
+				<Button color='inherit'
+					onClick={() => props.changeDStyle(Store.DisplayStyle.X10)} className={classes.x10Btn}>10個ずつ</Button>
+				<Button color='inherit'
+					onClick={() => props.changeDStyle(Store.DisplayStyle.X30)} className={classes.x30Btn}>30個ずつ</Button>
+				<Button color='inherit'
+					onClick={() => props.changeDStyle(Store.DisplayStyle.All)} className={classes.allBtn}>全て</Button>
+				<Button color='inherit'
+					onClick={() => props.openFilterDialog()} className={classes.filterBtn}>絞り込み</Button>
 			</Toolbar>
 		</AppBar>);
 		let begin = props.dstyle * props.page;
